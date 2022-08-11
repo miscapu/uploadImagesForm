b83becd61c08cb9da096aec58c043a1dbbe1ee00
miscapu <mignan2010@outlook.com>
2022-08-11 15:30:42 -0300

    Create upload folder and connect to mysql from db_config.php file

---
+++ db_config.php
@@ -0,0 +1,11 @@
+<?php
+$serverName =   'localhost';
+$username   =   'root';
+$passDB     =   'root';
+$dbname     =   'Override';
+
+
+$conn       =   mysqli_connect( $serverName, $username, $passDB, $dbname ) or die( 'Could not connect' );
+
+
+
